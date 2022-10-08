affc8389b3219829ac252d9db0d800841361936e
deesclouds <deescloud@protonmail.com>
2022-10-08 09:40:39 -0700

    add first solved coding challenge

---
+++ js/add-two-integers.js
@@ -0,0 +1,13 @@
+// Add Two Integers
+
+/* 
+
+Given two integers num1 and num 2, return the sum of the two integers.
+
+constraints:
+-100 <= num1, num2 <= 100
+*/
+
+var sum = function(num1, num2){
+    return num1 + num2;
+};
